dda2d3d6d72c62133b173624cf652c72015fdd6d
yeferson <yefersonvelandia4@gmail.com>
2022-02-19 18:05:23 -0500

    added validation in input form

--- app/Http/Controllers/RecetaController.php
+++ app/Http/Controllers/RecetaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Receta;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class RecetaController extends Controller
 {
@@ -35,7 +36,15 @@ class RecetaController extends Controller
      */
     public function store(Request $request)
     {
-        dd( $request->all() );
+        $data = request()->validate([
+            'titulo' => 'required|min:6'
+        ]);
+
+        DB::table('recetas')->insert([
+            'titulo' => $data['titulo']
+        ]);
+
+        return redirect()->action('App\Http\Controllers\RecetaController@index');
     }
 
     /**
