341b74bd2d83408c795a047012bca2e65187ec4b
yeferson <yefersonvelandia4@gmail.com>
2022-02-19 20:02:53 -0500

    added seeders user and recetas

--- database/migrations/2022_02_14_082512_create_recetas_table.php
+++ database/migrations/2022_02_14_082512_create_recetas_table.php
@@ -13,9 +13,20 @@ return new class extends Migration
      */
     public function up()
     {
+        Schema::create('categoria_receta', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->timestamps();
+        });
+
         Schema::create('recetas', function (Blueprint $table) {
             $table->id();
             $table->string('titulo'); 
+            $table->text('ingredientes'); 
+            $table->text('preparacion'); 
+            $table->string('imagen');
+            $table->unsignedBigInteger('user_id')->references('id')->on('users')->comment('El usuario que crea la receta'); 
+            $table->unsignedBigInteger('categoria_id')->index('categoria_id')->on('categoria_receta')->comment('categoria de la receta'); 
             $table->timestamps();
         });
     }
@@ -28,5 +39,6 @@ return new class extends Migration
     public function down()
     {
         Schema::dropIfExists('recetas');
+        Schema::dropIfExists('categoria_receta');
     }
 };
---
+++ database/seeders/CategoriasSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategoriasSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categoria_receta')->insert([
+            'nombre' => 'Comida Mexicana',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+        DB::table('categoria_receta')->insert([
+            'nombre' => 'Comida Italiana',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+        DB::table('categoria_receta')->insert([
+            'nombre' => 'Postre',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+        DB::table('categoria_receta')->insert([
+            'nombre' => 'Cortes',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+        DB::table('categoria_receta')->insert([
+            'nombre' => 'Ensaladas',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+        DB::table('categoria_receta')->insert([
+            'nombre' => 'Desayunos',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+    }
+}
